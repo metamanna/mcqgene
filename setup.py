8e6a9aee63638c07384c9d8b9af70cd07092fd04
metamanna <tamanna08aggarwal@gmail.com>
2025-09-03 00:33:00 +0530

    file updated

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
 # to store local package in virtual environment
+from setuptools import find_packages,setup
+
+setup(
+    name='mcqgenerator',
+    version='0.0.1',
+    author='tamanna aggarwal',
+    author_email='tamanna08aggarwal@gmail.com',
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    packages=find_packages()
+)
